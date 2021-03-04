5164641bd1008e5a6acc6f17864df93fa1c35fac
harry2002731 <why13601898034>
2021-03-04 21:58:15 +0800

    在SQLserver增添了Empty_database，Zero_Id函数

--- SQLserver.py
+++ SQLserver.py
@@ -1,12 +1,14 @@
 import sqlite3
 import config
 
+
 class DataServer_Sqlite3():
-    def __init__(self):
+    def __init__(self, db_name):
         self.curs = ''
         self.connection = ''
         self.header = ''
         self.data = ''
+        self.db_name = db_name
         self.Connecting_database()
 
     def Connecting_database(self):
@@ -20,8 +22,15 @@ class DataServer_Sqlite3():
     def Fetch_data(self):
         self.header = self.curs.description
         self.data = self.curs.fetchall()
-        print("")
 
-    def Create_table(self,name:str,input_header:str):
+    def Empty_database(self):
+        self.curs = self.curs.execute(f"DELETE FROM \"{self.db_name}\"")
+        self.connection.commit()
+
+    def Zero_Id(self):
+        self.curs = self.curs.execute(f"update sqlite_sequence set seq=0 where name='{self.db_name}'")
+        self.connection.commit()
+
+    def Create_table(self, name: str, input_header: str):
         """sample: input_header =int primary key,ts_code,symbol,name,area,industry,market,list_date"""
         self.Sql_execution(f"create table {name}({input_header})")
